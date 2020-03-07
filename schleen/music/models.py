91999dc22d476d2cf6bafa5ca32318cbf93c792b
stefan <sthefan.heller6341@gmail.com>
2020-03-06 23:17:52 -0500

    First models built and migrated. Bootstrap added. Static directories configured base template made. db configured.

--- schleen/music/models.py
+++ schleen/music/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+
+
+    usr_username = models.CharField(max_length=100)
+    usr_desc = models.TextField()
+    # cust_songs = [models.CharField(max_length=100)]
+    # cust_albums = [models.CharField(max_length=100)]
+    usr_img = models.ImageField(upload_to='pics')
+
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    date = models.DateField()
+
+
+class SongList(models.Model):
+    list = [Song]
--- schleen/music/views.py
+++ schleen/music/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import User
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def user(request):
+
+    users = User.objects.all()
+
+
+    return render(request, 'user.html', {'usrs':users})

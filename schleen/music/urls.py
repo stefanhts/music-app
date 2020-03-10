68d3e7db70ee86a3033815590b4ac503fa9fba48
stefan <sthefan.heller6341@gmail.com>
2020-03-10 00:05:38 -0400

    delete incorrect directory

--- schleen/music/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('user', views.user, name='user'),
-    path('topsongs', views.topsongs, name='topsongs')
-]
